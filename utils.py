144a7b70ffa54a89ba403684ab77ee12bcf2a084
lawson1225 <lawson9012225@gmail.com>
2018-12-30 22:10:12 -0600

    new focal

--- utils.py
+++ utils.py
@@ -135,60 +135,60 @@ class TFLogger(object):
         self.writer.add_summary(summary, step)
         self.writer.flush()
 
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25,gamma=2):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-
-    def forward(self, x, y):
-        '''Focal loss.
-        Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
-        Return:
-          (tensor) focal loss.
-        '''
-        t = Variable(y).cuda()  # [N,20]
-
-        p = x.sigmoid()
-        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
-        w = self.alpha*t + (1-self.alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
-        w = w * (1-pt).pow(self.gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
-
 # class FocalLoss(nn.Module):
-#     def __init__(self, gamma=2, alpha=0.25, size_average=True):
+#     def __init__(self, alpha=0.25,gamma=2):
 #         super(FocalLoss, self).__init__()
-#         self.gamma = gamma
 #         self.alpha = alpha
-#         if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
-#         if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
-#         self.size_average = size_average
+#         self.gamma = gamma
 #
-#     def forward(self, input, target):
-#         if input.dim()>2:
-#             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-#             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-#             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-#         target = target.view(-1,1)
+#     def forward(self, x, y):
+#         '''Focal loss.
+#         Args:
+#           x: (tensor) sized [N,D].
+#           y: (tensor) sized [N,].
+#         Return:
+#           (tensor) focal loss.
+#         '''
+#         t = Variable(y).cuda()  # [N,20]
 #
-#         logpt = F.log_softmax(input)
-#         logpt = logpt.gather(1,target)
-#         logpt = logpt.view(-1)
-#         pt = Variable(logpt.data.exp())
-#
-#         if self.alpha is not None:
-#             if self.alpha.type()!=input.data.type():
-#                 self.alpha = self.alpha.type_as(input.data)
-#             at = self.alpha.gather(0,target.data.view(-1))
-#             logpt = logpt * Variable(at)
-#
-#         loss = -1 * (1-pt)**self.gamma * logpt
-#         if self.size_average:
-#             return loss.mean()
-#         else:
-#             return loss.sum()
+#         p = x.sigmoid()
+#         pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+#         w = self.alpha*t + (1-self.alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
+#         w = w * (1-pt).pow(self.gamma)
+#         return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2, alpha=0.25, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha,(float,int,np.long)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1,1)
+
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type()!=input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0,target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
 
 # F1_loss
 # https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
