6a47a692949b7ab30c6d5bada33814ea877a6f82
lawson1225 <lawson9012225@gmail.com>
2018-12-31 22:15:39 -0600

    focalloss

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class DefaultConfigs(object):
     epochs = 50
     resume = True
     initial_checkpoint = '0'
-    gpus = "0,1,2,3"
+    gpus = "0"
     mode = 'train'
 
 config = DefaultConfigs()
--- utils.py
+++ utils.py
@@ -162,29 +162,26 @@ class FocalLoss(nn.Module):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha,(float,int,np.long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
         self.size_average = size_average
+        # if isinstance(alpha, (float, int)):
+        #     if self.alpha > 1:
+        #         raise ValueError('Not supported value, alpha should be small than 1.0')
+        #     else:
+        #         self.alpha = torch.Tensor([alpha, 1.0 - alpha])
+        # if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
 
-    def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1,target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
+    def forward(self, x, y):
+        # target = target.view(-1,1).long()
+        t = Variable(y).cuda()
+        p = x.sigmoid()
+        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+        logpt = torch.log(pt + 1e-10)
 
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+        # alpha = alpha if t > 0 else 1-alpha
+        alpha = self.alpha *t + (1-self.alpha)*(1-t)
+        loss = -1 * alpha * torch.pow((1 - pt), self.gamma) * logpt
 
-        loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average:
             return loss.mean()
         else:
